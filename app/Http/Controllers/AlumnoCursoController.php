5f364bd9f934557c79d4bf503c1a1c1754e26291
Dake2612 <47237464+Dake2612@users.noreply.github.com>
2020-09-21 20:08:20 -0500

    olis1

--- app/Http/Controllers/AlumnoCursoController.php
+++ app/Http/Controllers/AlumnoCursoController.php
@@ -12,13 +12,6 @@ class AlumnoCursoController extends Controller
     public function cursos()
     {
 
-        /*
-        SELECT curso.ID, curso.nombre, curso.salon_grado, curso.salon_seccion, CONCAT(persona.apellidoPaterno, ' ', persona.apellidoMaterno, ', ', persona.nombres) AS docente
-        FROM `curso`, `alumno-curso`, `persona`, `profesor-curso` 
-        WHERE (`alumno-curso`.DNI_ALUMNO = '87654321' AND `alumno-curso`.ID_CURSO = `curso`.ID AND 
-        `persona`.DNI = `profesor-curso`.DNI AND `profesor-curso`.ID = `curso`.ID);
-        */
-
         $cursos = AlumnoCurso::join('curso', 'alumno-curso.ID_CURSO', '=', 'curso.ID')
         ->join('profesor-curso', 'profesor-curso.ID', '=', 'curso.ID')
         ->join('persona', 'persona.DNI', '=', 'profesor-curso.DNI')
@@ -27,20 +20,10 @@ class AlumnoCursoController extends Controller
         ->where('alumno-curso.DNI_ALUMNO', '=', Auth::user()->DNI)->get();
 
         return view('estudiante.curso', compact('cursos'));
-        /*$cursos = AlumnoCurso::where([
-            ['DNI_ALUMNO', '=', Auth::user()->DNI]
-        ])->get();
-        return view('estudiante.curso', compact('cursos'));*/
     }
 
     public function notas()
     {
-        /*
-        SELECT curso.ID, curso.nombre, `alumno-curso`.nota 
-        FROM `curso`, `alumno-curso`
-        WHERE (`alumno-curso`.DNI_ALUMNO = '11111111' AND `alumno-curso`.ID_CURSO = `curso`.ID);
-        */
-
         $notas = AlumnoCurso::join('curso', 'alumno-curso.ID_CURSO', '=', 'curso.ID')
         ->select('curso.ID', 'curso.nombre', 'alumno-curso.nota')
         ->where('DNI_ALUMNO', '=', Auth::user()->DNI)->get();
